5cc9310f00560065080796e576b3ccde0adb9b7a
witchbl4des <menzo.smit-adam@epitech.eu>
2024-02-21 22:40:24 +0100

    init v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: [
-            "uploadthing.com",
-            "utfs.io",
-            "img.clerk.com",
-            "subdomain",
-            "files.stripe.com"
-        ],
-    },
-    reactStrictMode: false,
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/(main)/agency/(auth)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SignIn, SignUp } from "@clerk/nextjs";
-import React from "react";
-
-const Page = () => {
-    return <SignUp/>
-}
-
-export default Page;
